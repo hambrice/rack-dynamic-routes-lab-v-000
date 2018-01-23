37731f44167801438712d8c38fc4f7b8d8e61509
Brad Hambrice <hambrice@uab.edu>
2018-01-23 05:31:58 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+    end
+  end
+end
