f432b41c8c2270bf36d51e6519b6596345a1bcfb
Brad Hambrice <hambrice@uab.edu>
2018-01-23 05:57:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
       if item
-        resp.write "#{item.price}"
+        resp.write item.price
       else
         resp.write "Item not found."
         resp.status = 400
