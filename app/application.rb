00929fcf6203d0200cec244da1e0f271bee591bd
Brad Hambrice <hambrice@uab.edu>
2018-01-23 05:56:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 class Application
 
-@@items = [Item.new("apple", 8), Item.new("cheese", 4)]
+@@cart = [Item.new("apple", 8), Item.new("cheese", 4)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find {|i| i.name == item_name}
+      item = @@cart.find {|i| i.name == item_name}
       if item
         resp.write "#{item.price}"
       else
